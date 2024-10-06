e2e79ae40d72d53bbc9f8162d9749d06bbe20ae3
musin2 <muhsindaud75@gmail.com>
2024-10-06 23:36:45 +0300

    adds lab code

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response_body = response.content
+        return response_body
 
     def load_json(self):
-        pass
+        body_json = self.get_response_body()
+        body_in_dict  = json.loads(body_json)
+        return body_in_dict
